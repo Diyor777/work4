3e9edadc0d39b528a9a7085fc4adb4d6ef32e80e
Diyor777 <diki20202020@gmail.com>
2021-08-21 19:36:51 +0500

    commit work4

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/*
+styles.push(...items) – добавляет элементы в конец,
+styles.pop() – извлекает элемент из конца,
+styles.shift() – извлекает элемент из начала,
+styles.unshift(...items) – добавляет элементы в начало.
+*/
+
+let styles = ['Джаз', 'Блюз'];
+
+styles.push('Рок-н-ролл')
+styles.shift()
+styles.unshift('Реп', 'Регги')
+console.log(styles)
+
+// ---------------------------------------
+
+// 1.Введите в консоль слово Volvo из массива cars.
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// 2.Поменяте первое слово на Форд 
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+
+// 3. Введите в консоль количество элементов массива
+// const cars = ["Volvo", "Jeep", "Mercedes"];
---
+++ js/js1hw(1).js
@@ -0,0 +1,10 @@
+
+
+// 1.Введите в консоль слово Volvo из массива cars.
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// 2.Поменяте первое слово на Форд 
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+
+// 3. Введите в консоль количество элементов массива
+// const cars = ["Volvo", "Jeep", "Mercedes"];
